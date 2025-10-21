a6a7752902cc72c20e357945a5022d9a74f006ed
prathoseraaj <prathoseraaj0312@gmail.com>
2025-10-21 12:06:33 +0530

    feat: preprocessing

--- main.py
+++ main.py
@@ -17,3 +17,17 @@ import google.generativeai as genai
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse
 
+app = FastAPI()
+
+base_model = ResNet50(weights='imagenet', include_top=False, pooling='avg', input_shape=(224, 224, 3))
+gradcam_model = base_model
+
+gemini_api_key = os.getenv("gemini_api_key")
+genai.configure(api_key=gemini_api_key)
+
+def preprocess_for_similarity(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (224, 224))
+    img_array = np.expand_dims(img, axis=0)
+    return preprocess_input(img_array)
